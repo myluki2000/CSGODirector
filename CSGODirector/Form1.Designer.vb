41fa5c2636421b826595bed7442de0cf8b6a87b4
myluki2000 <lukas636@live.com>
2016-04-03 02:49:41 +0200

    Initial Release

---
+++ CSGODirector/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(79, 38)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(80, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Director On"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(127, 12)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(103, 20)
+        Me.NumericUpDown1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 26)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Switch delay after " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "round"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(240, 59)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label1 As Label
+End Class
---
+++ CSGODirector/Form1.vb
@@ -0,0 +1,71 @@
+﻿Imports CSGSI
+
+Public Class Form1
+    Dim WithEvents GSL As New GameStateListener(3550)
+    Dim CSGOMaximized As Boolean
+    Dim counter As Integer = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GSL.Start()
+    End Sub
+
+    Private Sub OnNewGameState(gs As GameState) Handles GSL.NewGameState
+        Try
+            If CheckBox1.Checked Then
+
+                If gs.Round.Phase = Nodes.RoundPhase.FreezeTime OrElse gs.Round.Phase = Nodes.RoundPhase.Over Then
+                    If CSGOMaximized AndAlso counter = NumericUpDown1.Value Then
+                        MaximizeWindow("chrome")
+                        MaximizeWindow("firefox")
+                        MaximizeWindow("vlc")
+
+                        SetCursorPos(0, 0)
+                        CSGOMaximized = False
+                    End If
+
+                    counter += 1
+                End If
+
+                If gs.Round.Phase = Nodes.RoundPhase.Live AndAlso CSGOMaximized = False Then
+                    MaximizeWindow("csgo")
+                    CSGOMaximized = True
+                    counter = 0
+                End If
+            End If
+        Catch e As Exception
+
+        End Try
+    End Sub
+
+    Private Sub MaximizeWindow(ByVal procName As String)
+        Dim procArray() As System.Diagnostics.Process = System.Diagnostics.Process.GetProcessesByName(procName)
+        If procArray.Length > 0 Then
+            Dim i As Integer = 0
+            For i = 0 To procArray.Length - 1
+                If Not procArray(i).MainWindowTitle = "" Then ShowPreviousInstance(procArray(i).MainWindowHandle)
+            Next
+        End If
+    End Sub
+
+    Private SW_RESTORE As Integer = 9
+    Private Declare Auto Function IsIconic Lib "user32" (ByVal hWnd As IntPtr) As Boolean
+    Private Declare Auto Function SetForegroundWindow Lib "user32" (ByVal hwnd As IntPtr) As Long
+    Private Declare Auto Function ShowWindow Lib "user32" (ByVal hWnd As IntPtr, ByVal nCmdShow As Integer) As IntPtr
+
+    Private Declare Function SetCursorPos Lib "user32" (ByVal X As Integer, ByVal Y As Integer) As Integer
+
+    Private Function ShowPreviousInstance(ByVal handle As IntPtr) As Boolean
+        If handle.ToInt32 <> IntPtr.Zero.ToInt32 Then
+            Try
+                If IsIconic(handle) Then
+                    ShowWindow(handle, SW_RESTORE)
+                End If
+                SetForegroundWindow(handle)
+                ShowPreviousInstance = True
+            Catch ex As System.Exception
+                ShowPreviousInstance = False
+            End Try
+        Else
+            ShowPreviousInstance = False
+        End If
+    End Function
+End Class
